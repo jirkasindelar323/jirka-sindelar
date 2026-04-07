7a2aec7d9483c25365326c78ec5ba5882d64914c
Jiri Sindelar <jirkasindelar323@gmail.com>
2026-04-08 04:56:20 +1000

    astro project init

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('colorButton');
-const title = document.getElementById('title');
-
-button.addEventListener('click', () => {
-    title.style.color = 'blue';
-});
