bfa8b641bf0f34ed63effaaaee185146bd2955f8
Jiri Sindelar <jirkasindelar323@gmail.com>
2026-04-09 15:17:59 +1000

    added Astro Island component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jirkasindelar.netlify.app",
@@ -13,5 +15,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), preact()]
 });
