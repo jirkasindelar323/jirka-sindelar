f95891ebedc25d9e508bcb7cc82f75a8682e0e8a
Jiri Sindelar <jirkasindelar323@gmail.com>
2026-04-10 18:53:30 +1000

    sitemap index added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react(),sitemap()]
 });
