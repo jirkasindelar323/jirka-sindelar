3c160c511a824712acafd73a931a4818ca6e3a0a
Jiri Sindelar <jirkasindelar323@gmail.com>
2026-04-12 16:28:39 +1000

    rename

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,7 +7,7 @@ export async function GET(context: APIContext) {
     .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
 
   return rss({
-    title: 'Jirka Sindelar | Blog',
+    title: 'Jiri Sindelar | Blog',
     description: 'Software engineering, building, and learning.',
     site: context.site!,
     items: posts.map((post) => ({
